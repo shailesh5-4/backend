00b395273538f9f0bebac8e3339e4b519cfdbd86
shailesh5-4 <108259289+shailesh5-4@users.noreply.github.com>
2025-03-16 00:13:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,5 +82,5 @@ app.get("/products", async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
